b7e780d39d76727ff01b089bb23fab525b19d999
Eugenia Rivero <126787554+eugenia3031@users.noreply.github.com>
2023-05-21 16:06:45 +0000

    last commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = useState([])
+	const [inputValue, setInputValue] = useState("")
+	const [actButon, setActButon] = useState(false)
+
+	function deleteTask(id){
+		setTask(task.filter((task,index) => {
+			return index !== id;
+		}))
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<h1>todos</h1>
+		<div className="todolist">
+			<form onSubmit={(e) => {
+				e.preventDefault()
+				setTask([...task,inputValue])
+				setInputValue ("")
+			}}
+			action="" method="get">
+				<div>
+					<input type="text" className="form-control border-0" placeholder="  What needs to be done?" onChange={e => setInputValue(e.target.value)} value={inputValue} />
+				</div>
+			</form>
+			<ul>
+				{task.map((task, i) => {
+					return <dd key={i} onMouseOver={() => setActButon(true)} onMouseOut={() => setActButon(false)}> {task} <a className={actButon ? "" : "disable"} onClick={() => {deleteTask(i)}}>X</a></dd>
+				})}
+			</ul>
+			<p>{task.length === 0 ? "No tasks added, add one" : task.length + " item left"}</p>
 		</div>
+		</>
 	);
 };
 
